6507131390c9437dc32b15ad91a0b5b775c223a1
YhyaGithub <yhyamobile57@gmail.com>
2023-09-05 14:25:43 +0300

    feat(layout): add main windows to the layout

--- client/main.c
+++ client/main.c
@@ -3,12 +3,27 @@
 #include <curses.h>
 
 int main(void) {
-  initscr();
-  read_messages();
-  setup_input();
-  endwin();
+	initscr();
+	clear();
+	read_messages();
+	setup_input();
+	refresh();
+	endwin();
 }
 
-void read_messages() {}
+void read_messages() {
+	WINDOW *messages_win = newwin(LINES - 3, 0, 0, 0);
 
-void setup_input() {}
+	box(messages_win, 0, 0);
+	mvwprintw(messages_win, 0, 0, "Messages");
+	wrefresh(messages_win);
+}
+
+void setup_input() {
+	WINDOW *input_win = newwin(3, 0, LINES - 3, 0);
+
+    box(input_win, 0, 0);
+    mvwprintw(input_win, 0, 0, "Input");
+    wmove(input_win, 1, 1);
+    wgetch(input_win);
+}
