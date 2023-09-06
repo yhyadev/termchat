e307f13431868a2a4bb3f74b44b0eecf7187edc6
YhyaGithub <yhyamobile57@gmail.com>
2023-09-06 14:33:38 +0300

    fix(vector): the capacity was increasing for no reason

--- client/vector.c
+++ client/vector.c
@@ -13,14 +13,17 @@ char_vector *init_char_vec() {
 }
 
 void push(char_vector *vec, char value) {
-    vec->values = realloc(vec->values, vec->capacity * sizeof(char));
-    vec->capacity *= 2;
-    vec->values[vec->size] = value;
-    vec->size++;
+	if (vec->size > vec->capacity) {
+		vec->values = realloc(vec->values, vec->capacity * sizeof(char));
+		vec->capacity *= 2;
+	}
+
+	vec->values[vec->size] = value;
+	vec->size++;
 }
 
 void pop(char_vector *vec) {
-    vec->size--;
-    vec->capacity /= 2;
-    vec->values = realloc(vec->values, vec->capacity * sizeof(char));
+	vec->size--;
+	vec->capacity /= 2;
+	vec->values = realloc(vec->values, vec->capacity * sizeof(char));
 }
